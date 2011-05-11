84a96c9ddb8a5f921b7a57232e37bb2ffb87587e
Jan Seiffert <kaffeemonster@googlemail.com>
2011-05-12 01:10:13 +0200

    add ARM v6 DSP version of slhash
    gives ~8%
    ============ orig ===============
    user    0m4.000s
    user    0m4.000s
    user    0m4.150s
    ============ v6 DSP ===============
    user    0m3.780s
    user    0m3.730s
    user    0m3.920s

--- arm/slhash.c
+++ arm/slhash.c
@@ -111,4 +111,79 @@ local void update_hoffset(Posf *p, uInt wsize, unsigned n)
         *p++ = (Pos)(m >= wsize ? m-wsize : NIL);
     } while (--n);
 }
+#elif defined(__GNUC__) && ( \
+        defined(__thumb2__)  && ( \
+            !defined(__ARM_ARCH_7__) && !defined(__ARM_ARCH_7M__) \
+        ) || ( \
+        !defined(__thumb__) && ( \
+            defined(__ARM_ARCH_6__)   || defined(__ARM_ARCH_6J__)  || \
+            defined(__ARM_ARCH_6T2__) || defined(__ARM_ARCH_6ZK__) || \
+            defined(__ARM_ARCH_7A__)  || defined(__ARM_ARCH_7R__) \
+        )) \
+    )
+#  define SOU32 (sizeof(unsigned int))
+#  define HAVE_SLHASH_VEC
+
+local noinline void update_hoffset_l(Posf *p, uInt wsize, unsigned n)
+{
+    do {
+        register unsigned m = *p;
+        *p++ = (Pos)(m >= wsize ? m-wsize : NIL);
+    } while (--n);
+}
+
+local void update_hoffset(Posf *p, uInt wsize, unsigned n)
+{
+    unsigned int i, vwsize;
+
+    if(unlikely(wsize > (1<<16)-1)) {
+        update_hoffset_l(p, wsize, n);
+        return;
+    }
+
+    vwsize = wsize | (wsize << 16);
+    if (ALIGN_DOWN_DIFF(p, SOU32)) {
+        register unsigned m = *p;
+        *p++ = (Pos)(m >= wsize ? m-wsize : NIL);
+        n--;
+    }
+
+    i  = n / (SOU32/sizeof(*p));
+    n %= SOU32/sizeof(*p);
+    if (i >= 4) {
+        unsigned int j = i / 4;
+        i %= 4;
+        asm (
+            "1:\n\t"
+            "subs %1, #1\n\t"
+            "ldmia %0, {r4-r7}\n\t"
+            "uqsub16 r4, r4, %2\n\t"
+            "uqsub16 r5, r5, %2\n\t"
+            "uqsub16 r6, r6, %2\n\t"
+            "uqsub16 r7, r7, %2\n\t"
+            "stmia %0!, {r4-r7}\n\t"
+            "bne 1b"
+            : /* 0 */ "=r" (p),
+              /* 1 */ "=r" (j)
+            : /* 2 */ "r" (vwsize),
+              /*  */ "0" (p),
+              /*  */ "1" (j)
+            : "r4", "r5", "r6", "r7"
+        );
+            unsigned int in = *(unsigned int *)p;
+            *(unsigned int *)p = in;
+    }
+    if (i) do {
+        unsigned int in = *(unsigned int *)p;
+        asm ("uqsub16 %0, %1, %2" : "=r" (in) : "r" (in), "r" (vwsize));
+        *(unsigned int *)p = in;
+        p += SOU32/sizeof(*p);
+    } while (--i);
+
+    if (unlikely(n)) {
+        register unsigned m = *p;
+        *p++ = (Pos)(m >= wsize ? m-wsize : NIL);
+    }
+}
+
 #endif
